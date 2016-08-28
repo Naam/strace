ad55a3bc69d2ea08536d11c7e38a29e8782ef1fe
Nahim El Atmani <nahim+dev@naam.me>
2016-08-28 23:19:21 +0200

    aarch64: Handle fault injection
    * linux/aarch64/set_scno (arch_set_scno): New function
    * linux/aarch64/set_error (set_error): Likewise

--- linux/aarch64/set_error.c
+++ linux/aarch64/set_error.c
@@ -1,13 +1,10 @@
 static long
 set_error(struct tcb *tcp)
 {
-	switch (aarch64_io.iov_len) {
-		case sizeof(aarch64_regs):
-			aarch64_regs.regs[8] = tcp->u_error;
-			return set_regs(tcp->pid);
-		case sizeof(arm_regs):
-			arm_regs.ARM_r7 = tcp->u_error;
-			return set_regs(tcp->pid);
-	}
-	return -1;
+	if (aarch64_io.iov_len == sizeof(arm_regs))
+		arm_regs.ARM_r0 = tcp->u_error;
+	else
+		aarch64_regs.regs[0] = tcp->u_error;
+
+	return set_regs(tcp->pid);
 }
--- linux/aarch64/set_scno.c
+++ linux/aarch64/set_scno.c
@@ -1,13 +1,10 @@
 static long
 arch_set_scno(struct tcb *tcp, int scno)
 {
-	switch (aarch64_io.iov_len) {
-		case sizeof(aarch64_regs):
-			aarch64_regs.regs[8] = scno;
-			return set_regs(tcp->pid);
-		case sizeof(arm_regs):
-			arm_regs.ARM_r7 = scno;
-			return set_regs(tcp->pid);
-	}
-	return -1;
+	if (aarch64_io.iov_len == sizeof(arm_regs))
+		arm_regs.ARM_r7 = scno;
+	else
+		aarch64_regs.regs[8] = scno;
+
+	return set_regs(tcp->pid);
 }
