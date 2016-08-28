273f14e90df270ea79fcb94c7b019349a86a9ced
Nahim El Atmani <nahim+dev@naam.me>
2016-08-28 16:07:11 +0200

    linux/mips/set_error.c (set_error): Handle LINUX_MIPSN32

--- linux/mips/set_error.c
+++ linux/mips/set_error.c
@@ -1,5 +1,16 @@
 static long
 set_error(struct tcb *tcp)
 {
-	return upoke(tcp->pid, REG_V0, tcp->u_error);
+	if (mips_REG_A3) {
+		mips_REG_A3 = -1;
+		mips_REG_V0 = tcp->u_error;
+	} else {
+#ifdef LINUX_MIPSN32
+		mips_REG_V0 = tcp->u_lrval;
+#else
+		mips_REG_V0 = tcp->u_rval;
+#endif
+	}
+
+	return set_regs(tcp->pid);
 }
