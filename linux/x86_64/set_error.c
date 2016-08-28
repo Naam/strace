b326b213e75a90a50f7bd840450d78a4f6417748
Nahim El Atmani <nahim+dev@naam.me>
2016-08-28 19:07:15 +0200

    x86_64: Handle fault injection for 64,32,x32 personalities
    * linux/x86_64/set_error.c (set_error): Handle x86_64 & i386 personalities
    * linux/x86_64/set_scno.c (arch_set_scno): Likewise

--- linux/x86_64/set_error.c
+++ linux/x86_64/set_error.c
@@ -1,6 +1,10 @@
 static long
 set_error(struct tcb *tcp)
 {
-	x86_64_regs.orig_rax = tcp->u_error;
+	if (x86_io.iov_len == sizeof(i386_regs))
+		i386_regs.orig_eax = tcp->u_error;
+	else
+		x86_64_regs.orig_rax = tcp->u_error;
+
 	return set_regs(tcp->pid);
 }
--- linux/x86_64/set_scno.c
+++ linux/x86_64/set_scno.c
@@ -1,6 +1,10 @@
 static long
 arch_set_scno(struct tcb *tcp, int scno)
 {
-	x86_64_regs.orig_rax = scno;
+	if (x86_io.iov_len == sizeof(i386_regs))
+		i386_regs.orig_eax = scno;
+	else
+		x86_64_regs.orig_rax = scno;
+
 	return set_regs(tcp->pid);
 }
